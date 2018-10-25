c0737aa7c5bdb53548a1705e0b7aa1511cb72933
goktugbasaran <35557780+goktugbasaran@users.noreply.github.com>
2018-10-25 22:00:05 +0300

    Login page is added but DB connection is not made. Home and About pages layouts are added. Made necessary route changes to server.py

--- server.py
+++ server.py
@@ -1,13 +1,27 @@
-from flask import Flask
-
+from flask import Flask,render_template,redirect,url_for,flash
+from forms import LoginForm
 
 app = Flask(__name__)
-
-
+app.config['SECRET_KEY'] = '9ioJbIGGH6ndzWOi3vEW' 
 @app.route("/")
+@app.route("/home")
 def home_page():
-    return "Hello, world!"
+    return render_template('home_page.html')
 
+@app.route("/about")
+def about_page():
+    return render_template('about_page.html')
+
+@app.route("/login")
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.tcnk.data == 'deneme_user' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login_page.html', title='Login', form=form)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
